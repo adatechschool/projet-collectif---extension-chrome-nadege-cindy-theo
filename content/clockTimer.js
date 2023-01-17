6e1a80e11bab210f7eb2fa36dcdfa5d5ced6e15f
Theo Seuge <theoseuge@neey.local>
2023-01-17 15:05:06 +0100

    timer update

--- content/clockTimer.js
+++ content/clockTimer.js
@@ -1,46 +1,76 @@
-// Programm for make timer
+// Countdown timer program
+// Button START/STOP/PAUSE/RESET
+const startButton = document.getElementById("start");
+const pauseButton = document.getElementById("pause");
+const resetButton = document.getElementById("reset");
 
-const startButton = document.getElementById('btn');
-const resetButton = document.getElementById('reset');
-const statusB = document.getElementById('status');
+// SELECT TIMER PART AND SPLIT
+const timer = document.getElementById("timer");
+const splited = timer.textContent.split(":");
 
-const timer = document.getElementById('timer');
+// STATUS OF TIMER
+const statusClock = document.getElementById("status");
 
-let startTime;
 
-function countUp() {
-    const date = new Date(Date.now() - startTime);
+// FUNCTION FOR MAKE DE COUNTDOWN
+function updateTimer() {
+    let seconds = parseInt(timer.textContent.split(":")[2]);
+    let minutes = parseInt(timer.textContent.split(":")[1]);
+    let hours = parseInt(timer.textContent.split(":")[0]);
 
-    const m = String(date.getMinutes()).padStart(2, '0');
-    const s = String(date.getSeconds()).padStart(2, '0');
-    const ms = String(date.getMilliseconds()).padStart(1, '0');
-
-    timer.textContent = `${m}:${s}`;
-
-    timeoutId = setTimeout(() => {
-        countUp();
-    }, 10);
+    if (seconds == 0) {
+        if (minutes == 0) {
+            if (hours == 0) {
+                clearInterval(interval);
+                return;
+            }
+            hours--;
+            minutes = 59;
+            seconds = 59;
+        } else {
+            minutes--;
+            seconds = 59;
+        }
+    } else {
+        seconds--;
+    }
+    if (seconds < 10) seconds = "0" + seconds;
+    if (minutes < 10) minutes = "0" + minutes;
+    if (hours < 10) hours = "0" + hours;
+    
+    timer.textContent = `${hours}:${minutes}:${seconds}`;
 }
 
-startButton.addEventListener('click', () => {
-    if (statusB.textContent === 'Stop') {
-        statusB.textContent = 'Start';
-        startButton.textContent = 'Start';
-        clearTimeout(timeoutId);
+// FUNCTION FOR START THE TIMER
+function startTimer() {
+    if (startButton.textContent == "START" 
+        && statusClock.textContent == "true" 
+        && timer.textContent != "00:00:00") {
+            interval = setInterval(updateTimer, 1000);
+            statusClock.textContent = "false";
+            startButton.textContent = "STOP";
+    } else if (startButton.textContent == "STOP") {
+        clearInterval(interval);
+        statusClock.textContent = "true";
+        startButton.textContent = "START";
     } else {
-        if (startTime !== undefined) {
-            statusB.textContent = 'Stop';
-            startButton.textContent = 'Stop';
-            countUp();
-        } else {
-            startTime = Date.now();
-            statusB.textContent = 'Stop';
-            startButton.textContent = 'Stop';
-            countUp();
-        }
+        console.log("Timer is already running");
     }
-})
+}
 
-resetButton.addEventListener('click', () => {
-    timer.textContent = '00:00.000';
-})
+// FUNCTION FOR PAUSE THE TIMER
+function pauseTimer() {
+    clearInterval(interval);
+}
+
+// FUNCTION FOR RESET THE TIMER
+function resetTimer() {
+    console.log("Reset");
+    timer.textContent = "00:00:00";
+    statusClock.textContent = "true";
+    startButton.textContent = "START";
+}
+
+// EVENT LISTENER FOR BUTTONS
+startButton.addEventListener("click", startTimer);
+resetButton.addEventListener("click", resetTimer);
