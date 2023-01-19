d57d44d18bf70e0c76e76f9a95c5eef66c7f0b41
Theo Seuge <theoseuge@neey.local>
2023-01-19 15:19:47 +0100

    modified background and fix timer

---
+++ background.js
@@ -0,0 +1,81 @@
+let intervalId; // Variable pour stocker l'identifiant de l'interval
+let count; // Variable pour stocker le temps restant
+let isPaused = false; // Variable pour stocker l'état de la pause
+
+// Récupère la durée sélectionnée dans le formulaire HTML
+const durationSelect = document.getElementById("duration");
+
+// Récupère les boutons de démarrage, de pause/reprendre et de réinitialisation
+const startButton = document.getElementById("start");
+const pauseResumeButton = document.getElementById("pause-resume");
+const resetButton = document.getElementById("reset");
+
+// Update le timer toutes les secondes
+function updateTimer() {
+  count--;
+  localStorage.setItem("count", count);
+  let minutes = Math.floor(count / 60);
+  let seconds = count % 60;
+  if (minutes < 10) minutes = "0" + minutes;
+  if (seconds < 10) seconds = "0" + seconds;
+  document.getElementById("timer").innerHTML = `${minutes}:${seconds}`;
+  if (count === 0) {
+    clearInterval(intervalId);
+  }
+}
+
+// Ajoute les écouteurs d'événement pour les boutons
+startButton.addEventListener("click", () => {
+  if (!intervalId) {
+    count = parseInt(durationSelect.value);
+    intervalId = setInterval(updateTimer, 1000);
+    localStorage.setItem("isPaused", false);
+    localStorage.setItem("isReset", false);
+  }
+});
+
+pauseResumeButton.addEventListener("click", () => {
+  if (!isPaused) {
+    clearInterval(intervalId);
+    isPaused = true;
+    localStorage.setItem("isPaused", true);
+    pauseResumeButton.textContent = "Reprendre";
+  } else {
+    intervalId = setInterval(updateTimer, 1000);
+    isPaused = false;
+    localStorage.setItem("isPaused", false);
+    pauseResumeButton.textContent = "Pause";
+  }
+});
+
+resetButton.addEventListener("click", () => {
+  clearInterval(intervalId);
+  intervalId = null;
+  localStorage.removeItem("count");
+  localStorage.setItem("isReset", true);
+  document.getElementById("timer").innerHTML = "00:00";
+  startButton.textContent = "Start";
+  pauseResumeButton.textContent = "Pause";
+});
+
+// Récupère la valeur de count, d'état de pause et de réinitialisation enregistrée dans le localStorage
+// et démarre le timer si nécessaire
+let savedCount = localStorage.getItem("count");
+let savedIsPaused = localStorage.getItem("isPaused");
+let savedIsReset = localStorage.getItem("isReset");
+
+if (savedCount && !savedIsReset) {
+  count = parseInt(savedCount);
+  isPaused = savedIsPaused === "true";
+  if (!isPaused) {
+    intervalId = setInterval(updateTimer, 1000);
+    pauseResumeButton.textContent = "Pause";
+  } else {
+    pauseResumeButton.textContent = "Reprendre";
+  }
+}
+
+// écouter l'évenement de fermeture de l'onglet ou du navigateur
+chrome.runtime.onSuspend.addListener(() => {
+  clearInterval(intervalId);
+});
--- content/social-media-blocker.js
+++ content/social-media-blocker.js
@@ -1,17 +1,15 @@
-// Our test word
-let word = "google";
 // List of sites we want to block
 let sitesBanned = ["google", "twitter", "facebook", "instagram", "mediapart"];
 
 sitesBanned.forEach(blockPage);
 
-function blockPage(){
-    for (i=0; i<=sitesBanned.length; i++) {
-        if (document.URL.includes(sitesBanned[i])){
-            var body = document.body;
-            body.parentNode.removeChild(body);
-        }
+function blockPage() {
+  for (let i = 0; i <= sitesBanned.length; i++) {
+    if (document.URL.includes(sitesBanned[i])) {
+      var body = document.body;
+      body.parentNode.removeChild(body);
     }
+  }
 }
 
 /* condition d'activation du script : si toggle on dans l'extension
