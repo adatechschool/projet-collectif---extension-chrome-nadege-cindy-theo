beb98bbec29a34fa8847c5e4ac4fa627b53d1ae6
LAPTOP-5BR0V3VG\Cindy B <cindybestaven44@outlook.fr>
2023-01-24 16:32:54 +0100

    Ajout de la fonctionnalité To Do List

---
+++ toDo.js
@@ -0,0 +1,65 @@
+//Création d'un bouton "fermer" et l'ajouter à chaque élément de la liste
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Supprimer des éléments de la liste actuel
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Ajouter un symbole "caché" lorsque l'on clique sur l'élément
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Création d'un nouvel élément de la liste en cliquant sur le bouton ajouté
+function newElement() {
+
+  let button = document.getElementById('addBtn');
+  
+
+  button.addEventListener('click', event => {
+  
+    
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("Tâche ajoutée");
+  } else {
+    document.getElementById("myList").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+ });
+}
+
+newElement();
